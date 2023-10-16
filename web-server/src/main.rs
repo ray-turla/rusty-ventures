08bde7c4672b63278e9ef6f05c6fa645d934cbaf
Raymond Turla <raymond.turla@paynamics.net>
2023-10-16 10:38:43 +0800

    Refactor http html response handlign

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -24,23 +24,17 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("./resources/success.html").unwrap();
-        let length = contents.len();
-    
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "./resources/success.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("./resources/404.html").unwrap();
-        let length = contents.len();
-    
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+        ("HTTP/1.1 404 NOT FOUND", "./resources/404.html")
+    };
 
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 
 }
