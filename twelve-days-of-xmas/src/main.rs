158fa4b47ce8d081437acc142ad9b34a905bb778
Raymond Turla <raymond.turla@paynamics.net>
2023-10-17 18:07:52 +0800

    refactor

--- twelve-days-of-xmas/src/main.rs
+++ twelve-days-of-xmas/src/main.rs
@@ -1,28 +1,42 @@
 fn main() {
     for i in 1..=12 {
-        let (position_suffix, _) = match_pattern(i);
+        let position_suffix= get_position_suffix(i);
         println!("On the {i}{position_suffix} of Christmas, my true love sent to me");
         for j in (1..=i).rev() {
-            let (_, item) = match_pattern(j);
+            let item = get_christmas_item(j);
             println!("{}", item);
         }
     }
 }
 
-fn match_pattern(pattern: i32) -> (&'static str, &'static str) {
-    return match pattern {
-        12 => ("th", "Twelve drummers drumming"),
-        11 => ("th", "Eleven pipers piping"),
-        10 => ("th", "Ten lords a-leaping"),
-        9 => ("th", "Nine ladies dancing"),
-        8 => ("th", "Eight maids a-milking"),
-        7 => ("th", "Seven swans a-swimming"),
-        6 => ("th", "Six geese a-laying"),
-        5 => ("th", "Five golden rings"),
-        4 => ("th", "Four calling birds"),
-        3 => ("rd", "Three french hens"),
-        2 => ("nd", "Two turtle doves, and"),
-        1 => ("st", "A partridge in a pear tree"),
-        _ => ("", "")
+fn get_christmas_item(num: i32) -> String {
+    return match num {
+        12 => "Twelve drummers drumming",
+        11 => "Eleven pipers piping",
+        10 => "Ten lords a-leaping",
+        9 => "Nine ladies dancing",
+        8 => "Eight maids a-milking",
+        7 => "Seven swans a-swimming",
+        6 => "Six geese a-laying",
+        5 => "Five golden rings",
+        4 => "Four calling birds",
+        3 =>  "Three french hens",
+        2 =>  "Two turtle doves, and",
+        1 =>  "A partridge in a pear tree",
+        _ => ""
+    }.to_string()
+}
+
+fn get_position_suffix(number: i32) -> String {
+    if (11..=13).contains(&(number % 100)) {
+        return "th".to_string();
     }
+
+    match number % 10 {
+        1 => "st",
+        2 => "nd",
+        3 => "rd",
+        _ => "th",
+    }
+    .to_string()
 }
