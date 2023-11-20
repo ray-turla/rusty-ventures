76f1d524e660930be776c58e6c851728ba6233df
Raymond Turla <raymond.turla@paynamics.net>
2023-11-20 11:44:15 +0800

    Add delete note

--- chapter-4/notes-app/src/main.rs
+++ chapter-4/notes-app/src/main.rs
@@ -1,11 +1,6 @@
-use crossterm::{
-    event::{poll, read, Event::{Key, Mouse, Resize, FocusGained, FocusLost, Paste, self}, KeyEvent, self, KeyEventState, KeyCode, KeyModifiers },
-    terminal::{enable_raw_mode, Clear, ClearType}, ExecutableCommand,
-};
 use std::{
     env, fs,
     io::{prelude::*, stdin, stdout},
-    time::Duration,
 };
 fn main() -> Result<(), String> {
     const VALID_ACTIONS: [&str; 4] = ["create", "read", "edit", "delete"];
@@ -29,6 +24,10 @@ fn main() -> Result<(), String> {
         }
         "edit" => {
             let _ = edit_note(&file_name)?;
+            Ok(())
+        }
+        "delete" => {
+            let _ = delete_note(&file_name)?;
             Ok(())
         }
         _ => {
@@ -70,8 +69,6 @@ fn validate_arg(arg: &String) -> bool {
 fn create_note(file_name: &String) -> Result<(), String> {
     println!("Enter Text (Press CTRL+D to apply changes): ");
     let mut buffer = String::new();
-    // let mut buffer = String::new();
-
     let stdin = stdin();
     {
         let mut handle = stdin.lock();
@@ -101,111 +98,57 @@ fn read_note(file_name: &String) -> Result<String, String> {
     }
 }
 
-fn edit_note(file_name: &String) -> Result<(), String> {
-    // Create a new file or open an existing one
-    let mut contents = read_note(&file_name)?;
-    let mut stdin = stdin();
-    let mut user_input = String::new();
-    let mut buffer = String::from(contents);
-    let mut stdout = stdout();
 
-    enable_raw_mode().unwrap();
-    print!("Enter your edits (press Esc when done): ");
-    // stdout.execute(Clear(ClearType::All)).unwrap();
+fn edit_note(file_name: &String) -> Result<String, String> {
+    println!("Enter Edit Text (Press CTRL+D to apply changes): ");
+    let content = read_note(&file_name)?;
+    let mut buffer = String::from(content);
+    let stdin = stdin();
+    let _ = stdout().flush();
+    {
+        let mut handle = stdin.lock();
 
-    stdout.flush().unwrap();
+        loop {
+            print!("=>");
+            let mut user_input = String::new();
+            let _ = match handle.read_line(&mut user_input) {
+                Ok(size) => size,
+                Err(e) => return Err(e.to_string())
+            };
 
-    loop {
-        if poll(Duration::from_millis(500)).unwrap() {
-            match read().unwrap() {
-                Key(event) => {
-                    event.modifiers;
-                    if event.state == KeyEventState::Down {
-                        match event.code {
-                            KeyCode::Esc => {
-                                break;
-                             }
-                            KeyCode::Enter => {
-                                user_input.push('\n');
-                                print!("\n");
-                                stdout.flush().unwrap();
-                            }
-                            KeyCode::Backspace => {
-                                if user_input.is_empty() {
-                                    buffer.pop();
-                                    println!("{buffer}");
-                                } else {
-                                    user_input.pop();
-                                    print!("\u{8} \u{8}");
-                                }
-                            }
-                            _ => {
-                                let c = match event.modifiers {
-                                    KeyModifiers::CONTROL => "".to_string(),
-                                    _ => String::from(format!("{:?}", event.code)),
-                                };
-                                user_input.push_str(&c);
-                                print!("{}", c);
-                                stdout.flush().unwrap();
-                            }
-                        }
-                    }
-                },
-                _ => {continue;}
+            if user_input.is_empty() {
+                break;
             }
+
+            for c in &mut user_input.chars() {
+                if c == '\x08' || c == '\x1B' || c == '\x7F' {
+                    buffer.pop();
+                } else if c == '\x04' {
+                    break;
+                } else {
+                    buffer.push(c);
+                }
+            }
+            println!("\n{buffer}");
         }
+
+        let mut file = match fs::File::create(file_name) {
+            Ok(f) => f,
+            Err(e) => return Err(e.to_string())
+        };
+
+        let _ = file.write_all(&mut buffer.as_bytes());
+
     }
-
-
-
-    Ok(())
+    Ok(String::from(""))
 }
 
-// fn edit_note(file_name: &String) -> Result<String, String> {
-//     println!("Enter Edit Text (Press CTRL+D to apply changes): ");
-//     let content = read_note(&file_name)?;
-//     let mut buffer = String::from(content);
-//     // let mut buffer = String::new();
-
-//     let stdin = stdin();
-//     let _ = stdout().flush();
-//     {
-//         let mut handle = stdin.lock();
-
-//         loop {
-//             print!("=>");
-//             let mut user_input = String::new();
-//             let _ = match handle.read_line(&mut user_input) {
-//                 Ok(size) => size,
-//                 Err(e) => return Err(e.to_string())
-//             };
-
-//             if user_input.is_empty() {
-//                 break;
-//             }
-
-//             for c in &mut user_input.chars() {
-//                 if c == '\x08' || c == '\x1B' || c == '\x7F' {
-//                     buffer.pop();
-//                 } else if c == '\x04' {
-//                     break;
-//                 } else {
-//                     buffer.push(c);
-//                     // print!("{buffer}");
-//                 }
-//             }
-//             println!("\n{buffer}");
-//             // stdout().lock().write(buffer.as_bytes());
-//             // stdout().flush();
-//         }
-
-//         let mut file = match fs::File::create(file_name) {
-//             Ok(f) => f,
-//             Err(e) => return Err(e.to_string())
-//         };
-
-//         let _ = file.write_all(&mut buffer.as_bytes());
-
-//     }
-//     Ok(String::from(""))
-// }
+fn delete_note(file_name: &String) -> Result<String, String> {
+    match fs::remove_file(file_name) {
+        Ok(_) => {
+            println!("File Deleted!");
+            Ok(file_name.to_string())
+        },
+        Err(e) => Err(e.to_string()),
+    }
+}
